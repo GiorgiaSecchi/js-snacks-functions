2579918274b730fde9a0356b8e8a9956a795ea35
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-21 15:36:07 +0200

    1 - output

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,17 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
 const getInitials = (array) => {
-    let initials = []
-    for (let i = 0; i < array.length; i++) 
-}
-
+  // variabile array vuoto in cui si aggiungeranno le iniziali
+  let initials = [];
+  // ciclo per scorrere la lunghezza dell'array
+  for (let i = 0; i < array.length; i++) {
+    // aggiungere alla variabile "initials" le prime lettere di ogni elemento nell'array
+    initials.push(array[i][0]);
+  }
+  return initials;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const initials = getInitials(names);
+console.log(initials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
