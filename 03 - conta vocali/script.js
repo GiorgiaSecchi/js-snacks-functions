d7ddcf9694b89c895894008298e24956aa64ae87
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-21 18:14:45 +0200

    3 - for

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+const vowelWord = (string) => {
+  const vowel = "aeiou";
+  for (let i = 0; i < string.length; i++)
+    if (vowel.includes(string[i])) {
+      return string[i];
+    }
+};
 
+test = vowelWord(word);
+console.log(test);
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
